26f62bbc0923636718ef752dfb12434cec7b10d8
Job Schipper <j.schipper@focum.nl>
2015-04-20 17:01:10 +0200

    added absolute path for audiofiles. its now possible to use this application from a different working dir

--- push-it.sh
+++ push-it.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
+SCRIPT_DIR=$(cd `dirname "${BASH_SOURCE[0]}"` && pwd)
 
 command -v cvlc >/dev/null 2>&1 || { echo >&2 "cvlc is not installed. not pushing it real good :("; exit 1; }
 
-ls audio |sort -R |tail -1 |while read file; do
+ls $SCRIPT_DIR/audio |sort -R |tail -1 |while read file; do
 	echo "push it real good!"
-	(cvlc --play-and-exit audio/$file > /dev/null 2>&1) 
+	(cvlc --play-and-exit $SCRIPT_DIR/audio/$file > /dev/null 2>&1) 
 done
